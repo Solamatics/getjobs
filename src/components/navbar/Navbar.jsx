0b775e31e5785a86491619c6ff7e8ff06f916b70
Olusola Afikode <afikodeolaitan@gmail.com>
2023-02-28 07:06:06 +0100

    add seller dropdown

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,36 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import "./navbar.scss";
 
 const Navbar = () => {
+  const [active, setActive] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const isActive = () => {
+    window.scrollY > 0 ? setActive(true) : setActive(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", isActive);
+
+    return () => {
+      window.removeEventListener("scroll", isActive);
+    };
+  }, []);
+
+  const currentUser = {
+    id: 1,
+    username: "Olusola Afikode",
+    isSeller: true,
+  };
+
   return (
-    <div className="navbar">
+    <div className={active ? "navbar active" : "navbar"}>
       <div className="container">
         <div className="logo">
+          {/* <Link to="/">
+            <span className="text">fiverr</span>
+          </Link> */}
           <span className="text">fiverr</span>
           <span className="dot">.</span>
         </div>
@@ -13,10 +39,41 @@ const Navbar = () => {
           <span>Explore</span>
           <span>English</span>
           <span>Sign in</span>
-          <span>Become a Seller</span>
-          <button>Join</button>
+          {!currentUser?.isSeller ? <span>Become a Seller</span> : null}
+          {!currentUser?.isSeller && <button>Join</button>}
+          {currentUser?.isSeller && (
+            <div className="user" onClick={() => setOpen(!open)}>
+              <img
+                src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <span>{currentUser.username}</span>
+              {open ? (
+                <div className="options">
+                  {currentUser?.isSeller && (
+                    <>
+                      <span>Gigs</span>
+                      <span>Add New Gigs</span>
+                    </>
+                  )}
+                  <span>Orders</span>
+                  <span>Messgaes</span>
+                  <span>Logout</span>
+                </div>
+              ) : null}
+            </div>
+          )}
         </div>
       </div>
+      {active && (
+        <>
+          <hr />
+          <div className="menu">
+            <span>Test</span>
+            <span>Test2</span>
+          </div>
+        </>
+      )}
     </div>
   );
 };
