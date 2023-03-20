7a83e4e38c5e6a366f6ec02bcb38216f27f39b93
Olusola Afikode <afikodeolaitan@gmail.com>
2023-03-20 11:44:29 +0100

    implement api for user authentication

--- backend/api/controllers/user.controller.js
+++ backend/api/controllers/user.controller.js
@@ -1,17 +1,12 @@
 import User from "../models/user.model.js";
-import jwt from "jsonwebtoken";
 
 export const deleteUser = async (req, res) => {
   const user = await User.findById(req.params.id);
-  const token = req.cookies.accessToken;
-  if (!token) return res.status(401).send("You are not authorized");
 
-  jwt.verify(token, process.env.JWT_KEY, async (err, payload) => {
-    if (payload.id !== user._id.toString()) {
-      console.log("String", user._id.toString());
-      return res.status(401).send("You can delete only your account!!!");
-    }
-    await User.findByIdAndDelete(req.params.id);
-    res.status(200).send("User deleted");
-  });
+  if (req.userId !== user._id.toString()) {
+    console.log("String", user._id.toString());
+    return res.status(401).send("You can delete only your account!!!");
+  }
+  await User.findByIdAndDelete(req.params.id);
+  res.status(200).send("User deleted");
 };
